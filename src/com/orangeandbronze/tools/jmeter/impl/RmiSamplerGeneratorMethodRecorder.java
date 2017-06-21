827c618f7d44f4636b50d101c2768c0432ee1c8b
JM Ibanez <jmibanez@gmail.com>
2017-06-21 10:55:47 +0800

    Bind a BeanShell post-processor for calls with returned remotes.
    If a method call is detected to a Remote anywhere in the object graph
    of the return value, we create a BeanShell post-processor element with
    a script that registers the detected remotes.

--- src/com/orangeandbronze/tools/jmeter/impl/RmiSamplerGeneratorMethodRecorder.java
+++ src/com/orangeandbronze/tools/jmeter/impl/RmiSamplerGeneratorMethodRecorder.java
@@ -8,12 +8,17 @@
 package com.orangeandbronze.tools.jmeter.impl;
 
 import java.rmi.RemoteException;
+import java.util.Map;
 import com.orangeandbronze.tools.jmeter.MethodCallRecord;
 import com.orangeandbronze.tools.jmeter.MethodRecorder;
 import com.orangeandbronze.tools.jmeter.NativeRmiProxyController;
 import com.orangeandbronze.tools.jmeter.RMISampler;
-import org.apache.jmeter.testelement.TestElement;
 import com.orangeandbronze.tools.jmeter.gui.RMISamplerGUI;
+import org.apache.jmeter.extractor.BeanShellPostProcessor;
+import org.apache.jmeter.protocol.java.sampler.BeanShellSampler;
+import org.apache.jmeter.testbeans.gui.TestBeanGUI;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.testelement.property.BooleanProperty;
 import com.orangeandbronze.tools.jmeter.RMIRemoteObjectConfig;
 import com.orangeandbronze.tools.jmeter.util.ScriptletGenerator;
 import org.apache.commons.logging.Log;
@@ -104,6 +109,25 @@ public class RmiSamplerGeneratorMethodRecorder
         return sb.toString();
     }
 
+    private String createResultRegisterScript(final MethodCallRecord r) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Object ret = prev.getReturnValue();\n");
+        sb.append("com.orangeandbronze.tools.jmeter.InstanceRegistry reg = vars.getObject(\"RMIRemoteObject.instances\");");
+        sb.append("\n");
+        sb.append("// REPLACE_ME: name of remote in registry \n");
+        sb.append("// ret: actual object path to Remote \n");
+        Map<String, String> paths = r.getRemotePathsInReturn();
+        for (String handle: paths.keySet()) {
+            String pathKey = paths.get(handle);
+            sb.append("reg.registerRmiInstance(\"");
+            sb.append(handle);
+            sb.append("\", ret");
+            sb.append(pathKey);
+            sb.append(");");
+        }
+        return sb.toString();
+    }
+
 
     // Implementation of com.orangeandbronze.tools.jmeter.MethodRecorder
 
@@ -116,10 +140,28 @@ public class RmiSamplerGeneratorMethodRecorder
     public void recordCall(final MethodCallRecord r)
         throws RemoteException {
         RMISampler sampler = new RMISampler();
+        BeanShellPostProcessor retValProc = null;
+        sampler.setProperty(TestElement.TEST_CLASS, RMISampler.class.getName());
         sampler.setProperty(TestElement.GUI_CLASS, RMISamplerGUI.class.getName());
+        sampler.setTargetName(r.getTarget());
         sampler.setMethodName(r.getMethod());
         sampler.setArgumentsScript(createArgumentsScript(r));
-        target.deliverSampler(sampler, r);
+
+        if(r.isRemoteReturned()) {
+            retValProc = new BeanShellPostProcessor();
+            retValProc.setProperty(TestElement.TEST_CLASS,
+                                   BeanShellPostProcessor.class.getName());
+            retValProc.setProperty(TestElement.GUI_CLASS,
+                                   TestBeanGUI.class.getName());
+            // Add postprocessor to register remote instance return value
+            String script = createResultRegisterScript(r);
+            retValProc.setName("Save remote in return value");
+            retValProc.setProperty(new BooleanProperty(BeanShellSampler.RESET_INTERPRETER, true));
+            retValProc.setProperty("script", script);
+            retValProc.setScript(script);
+            sampler.addTestElement(retValProc);
+        }
+        target.deliverSampler(sampler, retValProc, r);
     }
 
 }
