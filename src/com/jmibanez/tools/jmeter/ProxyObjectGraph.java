64f9fe534d236cc2550f9487efc050da10f365df
JM Ibanez <jmibanez@gmail.com>
2017-07-20 01:42:27 +0800

    Revert "Merge pull request #6 from jmibanez/feature/sampler-tagging"
    This reverts commit 29a6d9ce08f99d02bb87d18205883e58b336c080, reversing
    changes made to 6ad3cf20a9c1f292563d7c986b680d50660e7269.
    
    Completely discard this approach to sampler tagging for a much better
    (readable) solution.

--- src/com/jmibanez/tools/jmeter/ProxyObjectGraph.java
+++ src/com/jmibanez/tools/jmeter/ProxyObjectGraph.java
@@ -21,7 +21,7 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import static com.jmibanez.tools.jmeter.util.UniqueNameFactory.buildInstanceName;
+import static com.jmibanez.tools.jmeter.util.InstanceHandleFactory.buildInstanceName;
 import static com.jmibanez.tools.jmeter.util.ReflectionUtil.getFieldsUpTo;
 
 
--- src/com/jmibanez/tools/jmeter/impl/RmiSamplerGeneratorMethodRecorder.java
+++ src/com/jmibanez/tools/jmeter/impl/RmiSamplerGeneratorMethodRecorder.java
@@ -24,8 +24,6 @@ import com.jmibanez.tools.jmeter.util.ScriptletGenerator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import static com.jmibanez.tools.jmeter.util.UniqueNameFactory.generateKeyForMethod;
-
 /**
  * Describe class RmiSamplerGeneratorMethodRecorder here.
  *
@@ -81,12 +79,8 @@ public class RmiSamplerGeneratorMethodRecorder
         }
 
         ScriptletGenerator gen = new ScriptletGenerator();
-        String genKey = generateKeyForMethod(record);
 
         StringBuilder sb = new StringBuilder();
-        sb.append("// $Tag '");
-        sb.append(genKey);
-        sb.append("'\n");
         sb.append("setAccessibility(true);\nmethodArgs ( ) {\n");
 
         String[] argVarnames = new String[args.length];
